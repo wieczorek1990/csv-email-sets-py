d15f33a635acf90463d56c6fb5fbde46c6e860de
Łukasz Wieczorek <lwieczorek@apptension.com>
2016-10-19 14:42:13 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import csv
+
+def get_emails(filename):
+  with open(filename) as fp:
+    reader = csv.DictReader(fp)
+    emails = map(lambda row: row['email'], reader)
+  return set(emails)
+
+a_emails = get_emails('a.csv')
+b_emails = get_emails('b.csv')
+
+print a_emails, b_emails
+print a_emails - b_emails, b_emails - a_emails, a_emails | b_emails, a_emails & b_emails
+
